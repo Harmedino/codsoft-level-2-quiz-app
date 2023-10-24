faa8d18330b1f32536d11fcbad4154e823c5c669
Harmedino <adamsahmed191@gmail.com>
2023-10-24 16:42:23 +0100

    firebase initilized

---
+++ JAVASCRIPT/register.js
@@ -0,0 +1,23 @@
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBYqhdVwi2yDV0DmP1iZTBtDmiXcvVA1iQ",
+    authDomain: "quiz-app-197e0.firebaseapp.com",
+    projectId: "quiz-app-197e0",
+    storageBucket: "quiz-app-197e0.appspot.com",
+    messagingSenderId: "246587546388",
+    appId: "1:246587546388:web:0d5a3e0d6fd6d9c1580001",
+    measurementId: "G-JDWQ596NBY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
