33cb0208e2c14abf4fb20db1b19ddea9344fef46
Harmedino <adamsahmed191@gmail.com>
2023-10-24 19:27:19 +0100

    registeration finished

--- JAVASCRIPT/register.js
+++ JAVASCRIPT/register.js
@@ -103,7 +103,8 @@ form.addEventListener('submit', async (event) => {
         try {
             // Check for unique username here (you need to implement this)
             if (await isUsernameUnique(username)) {
-                await registerUser(firstname,lastname, username, email, password);
+                await registerUser(firstname, lastname, username, email, password);
+                window.location = "../HTML/login.html";
             } else {
                 usernameInput.classList.add('invalid-input');
                 usernameError.textContent = 'Username already exist';
@@ -118,12 +119,14 @@ form.addEventListener('submit', async (event) => {
     }
 });
 
-  async function registerUser(firstname,lastname,username, email, password) {
+async function registerUser(firstname, lastname, username, email, password) {
     try {
+        // Attempt to create a new user account with email and password
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
-
+    
         if (user) {
+            // If the user account is successfully created, add user information to the Firestore database
             const userRef = await setDoc(
                 doc(db, "users", user.uid),
                 {
@@ -134,43 +137,49 @@ form.addEventListener('submit', async (event) => {
                 },
                 { merge: true }
             );
-
         }
     } catch (error) {
-       
+        // Handle Firebase Authentication errors
         if (error.code && error.message) {
             switch (error.code) {
                 case 'auth/invalid-email':
+                    // Invalid email format
                     emailInput.classList.add('invalid-input');
                     document.getElementById('emailError').textContent = error.message;
                     break;
                 case 'auth/user-disabled':
+                    // User account is disabled
                     emailInput.classList.add('invalid-input');
                     document.getElementById('emailError').textContent = error.message;
                     break;
                 case 'auth/user-not-found':
+                    // User not found
                     emailInput.classList.add('invalid-input');
                     document.getElementById('emailError').textContent = error.message;
                     break;
                 case 'auth/email-already-in-use':
+                    // Email is already in use
                     emailInput.classList.add('invalid-input');
                     document.getElementById('emailError').textContent = 'Email is already in use.';
                     break;
                 case 'auth/operation-not-allowed':
+                    // Operation not allowed (e.g., email/password accounts not enabled)
                     emailInput.classList.add('invalid-input');
                     document.getElementById('emailError').textContent = error.message;
                     break;
                 case 'auth/weak-password':
+                    // Weak password (e.g., too short)
                     passwordInput.classList.add('invalid-input');
                     document.getElementById('passwordError').textContent = error.message;
                     break;
                 default:
-                    // Handle other errors or display a generic error message
+                    // Handle other Firebase Authentication errors or display a generic error message
                     break;
             }
         }
     }
 }
+
 
 async function isUsernameUnique(username) {
     try {
